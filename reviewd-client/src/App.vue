d0f49311ddedd4867820269569f0507e4db50852
Eunyeol <ney9083@gmail.com>
2022-05-20 13:23:46 +0900

    Master Branch 초기세팅

---
+++ reviewd-client/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+};
+</script>
+
+<style lang="scss" scoped>
+* {
+  font-family: Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto,
+    "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR",
+    "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol",
+    sans-serif;
+  font-size: 100px;
+}
+</style>
