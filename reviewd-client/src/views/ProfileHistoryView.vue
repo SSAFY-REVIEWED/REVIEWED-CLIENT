fe3f1d44fd0c072c0d0343cf786ddedfb905e2aa
Eunyeol <ney9083@gmail.com>
2022-05-25 01:31:08 +0900

    Feat: heatmap 설정

--- reviewd-client/src/views/ProfileHistoryView.vue
+++ reviewd-client/src/views/ProfileHistoryView.vue
@@ -1,17 +1,64 @@
 <template>
-  <div>
-    여기는 바로 역사 호호호호 내가 바로 역사다라라라림우
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Est adipisci odio
-      dolores nemo nobis corporis? At officia, minima porro facilis beatae
-      possimus aliquid ad voluptate eum maiores cupiditate omnis est!
-    </p>
+  <div class="w-full h-96">
+    <calendar-heatmap
+      :values="value"
+      :end-date="today"
+      :range-color="[
+        '#ebedf0',
+        '#dae2ef',
+        '#c0ddf9',
+        '#73b3f3',
+        '#3886e1',
+        '#17459e',
+      ]"
+      :max="10"
+      tooltip-unit="stars"
+    />
   </div>
 </template>
 
 <script>
+import "vue-calendar-heatmap/dist/vue-calendar-heatmap.css";
+import { CalendarHeatmap } from "vue-calendar-heatmap";
+import Vue from "vue";
+Vue.component("calendarHeatmap", CalendarHeatmap);
+
 export default {
   name: "profileHistoryView",
+  components: {
+    CalendarHeatmap,
+  },
+  data() {
+    return {
+      endDate: null,
+      value: [
+        { date: "2021-9-24", count: 1 },
+        { date: "2021-9-25", count: 2 },
+        { date: "2021-9-26", count: 3 },
+        { date: "2021-9-26", count: 4 },
+        { date: "2021-9-27", count: 5 },
+        { date: "2021-9-28", count: 6 },
+        { date: "2021-9-29", count: 7 },
+        { date: "2021-09-30", count: 8 },
+        { date: "2021-10-01", count: 9 },
+        { date: "2021-10-02", count: 10 },
+        { date: "2021-10-03", count: 11 },
+        { date: "2021-10-04", count: 12 },
+        { date: "2021-10-05", count: 13 },
+        { date: "2021-10-06", count: 8 },
+        { date: "2022-05-24", count: 100 },
+        { date: "2022-05-25", count: 40 },
+      ],
+    };
+  },
+  computed: {
+    today() {
+      const day = new Date();
+      const date = day.toLocaleDateString();
+      return date;
+    },
+  },
+  created() {},
 };
 </script>
 
