3c85400a3b84bed5674d8e823dfb61de36d8b6be
Eunyeol <ney9083@gmail.com>
2022-05-24 09:48:07 +0900

    Fix: rankView 에러 수정

--- reviewd-client/vue.config.js
+++ reviewd-client/vue.config.js
@@ -4,14 +4,5 @@ module.exports = defineConfig({
   devServer: {
     proxy: "http://localhost:8000",
   },
-  css: {
-    loaderOptions: {
-      sass: {
-        data: `
-          @import "@/styles/_variables.scss";
-          @import "@/styles/_mixins.scss";
-        `,
-      },
-    },
-  },
+  
 });
