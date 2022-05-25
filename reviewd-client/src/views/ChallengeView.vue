dafd14365b71e3efdedbea1407f168b061ba3232
Eunyeol <ney9083@gmail.com>
2022-05-25 17:34:21 +0900

    Style: CSS 수정

--- reviewd-client/src/views/ChallengeView.vue
+++ reviewd-client/src/views/ChallengeView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="max-w-container mx-auto pt-0 px-9 min-h-screen">
+  <div class="max-w-container mx-auto pt-0 px-9 min-h-screen  mt-20">
     <h1 class="text-h3 font-bold">CHALLENGE</h1>
     <h2 class="text-h4 mb-9">영화 리뷰에 새로운 도전을 해보세요</h2>
     <EventChallenge />
